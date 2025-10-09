5936cc8638d9e90d582908febf7355c361646253
Trippin Sage <92277801+trippinsage@users.noreply.github.com>
2025-10-09 14:36:25 -0230

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -369,3 +369,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500);
   });
 });
+
+  // Service Worker
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => console.log('Service Worker registered:', registration))
+      .catch(error => console.error('Service Worker registration failed:', error));
+  });
+} 
--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,12 @@ const urlsToCache = [
   '/assets/js/script.js',
   '/assets/img/riffs-cutout.webp',
   '/assets/img/riffs-classic.jpg',
-  '/assets/img/riffs-classic-640.jpg'
+  '/assets/img/riffs-classic-640.jpg',
+  '/assets/img/riffs-background.webp',
+  '/assets/img/riffs-challange.png',
+  '/404.html',
+  '/assets/legal/legal.html',
+  '/sitemap.xml'
 ];
 
 self.addEventListener('install', event => {
@@ -21,6 +26,21 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
